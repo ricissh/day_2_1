63d22150d4153c29910bbf506cf1d5bf86994b7a
ricissh <rihards.stegmanis@icloud.com>
2018-05-07 20:01:22 +0300

    Day 2
    Otrā diena , projekta izveide

---
+++ day_2_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace day_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+
+            String diena;
+                String gads;
+            String menesis;
+
+            Console.WriteLine("ievadi savu dzimsanas dienu");
+            diena = Console.ReadLine();
+            Console.WriteLine("ievadi savu dzimsanas menesi");
+            menesis = Console.ReadLine();
+            Console.WriteLine("ievadi savu dzimsanas gadu");
+            gads = Console.ReadLine();
+            Console.WriteLine("sveiki tava dzimsanas diena ir "+ diena + menesis + gads);
+            Console.ReadLine();
+        }
+    }
+}         
